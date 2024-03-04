20959e39369affd8bbd8c6b26ea147749102fa99
Alinovskii Aleksandr <a.alinovskii@hh.ru>
2024-03-04 13:51:06 +0700

    first commit

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -3,6 +3,8 @@ import clsx from 'clsx';
 
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
+
+
     <span
       className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
